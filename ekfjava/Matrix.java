fa778278c17f7ed8968e28123dee53730dace103
Frank Moreno <frankmoreno1993@gmail.com>
2017-08-01 01:42:30 -0500

    renaming package

--- ekfjava/Matrix.java
+++ ekfjava/Matrix.java
@@ -1,35 +1,4 @@
-/*
-(The MIT License.)
-
-Copyright (c) 2009, Kevin Lacker.
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
-
-/*
- * This is a Java port of original C code by Kevin Lacker.
- * https://github.com/lacker/ikalman
- * 
- * Ported by Andrey Novikov, 2013
- */
-
-package com.androzic.utils;
+package kalmangps;
 
 public class Matrix {
 	/* Dimensions */
@@ -251,13 +220,13 @@ public class Matrix {
 
 	/*
 	 * Uses Gauss-Jordan elimination.
-	 * 
+	 *
 	 * The elimination procedure works by applying elementary row operations to
 	 * our input matrix until the input matrix is reduced to the identity
 	 * matrix. Simultaneously, we apply the same elementary row operations to a
 	 * separate identity matrix to produce the inverse matrix. If this makes no
 	 * sense, read wikipedia on Gauss-Jordan elimination.
-	 * 
+	 *
 	 * This is not the fastest way to invert matrices, so this is quite possibly
 	 * the bottleneck.
 	 */
